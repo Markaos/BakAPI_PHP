cb03f03b6a5e63ead2502a5e7749944219e5e21d
Markaos <markaos@email.cz>
2016-08-25 16:54:26 +0200

    Added BakAPI initialization

--- bakapi.php
+++ bakapi.php
@@ -3,7 +3,11 @@ require_once "BakAPI.php";
 
 // It's time to load custom files
 $settings = \Markaos\BakAPI\Util::getSettings();
-foreach ($settings["include"] as $file) {
-  require_once $file;
+if(isset($settings["include"])) {
+  foreach ($settings["include"] as $file) {
+    require_once $file;
+  }
 }
+
+\Markaos\BakAPI\Util::initBakAPI();
 ?>
