01b18c7a1de30880083e65b5be21a89f3ab5d9c0
Markaos <markaos@email.cz>
2016-08-29 17:55:00 +0200

    Added DiffUtil::getChecksum()

--- DiffUtil.php
+++ DiffUtil.php
@@ -113,6 +113,71 @@ namespace Markaos\BakAPI {
         }
       }
     }
+
+    // Compute SHA-256 checksum of BakAPI section
+    public static function getChecksum($db, $section) {
+      $str = "";
+      switch($section) {
+        case BAKAPI_SECTION_GRADES:
+          foreach($db[$section] as $item) {
+            $str .= $item["subject"] . $item["title"] . $item["description"] .
+                    $item["grade"] . $item["weight"] . $item["date"];
+          }
+          break;
+        case BAKAPI_SECTION_SUBJECTS:
+          foreach($db[$section] as $item) {
+            $str .= $item["name"] . $item["teachers"] . $item["emails"] .
+                    $item["short"];
+          }
+          break;
+        case BAKAPI_SECTION_MESSAGES:
+          foreach($db[$section] as $item) {
+            $str .= $item["from"] . $item["contents"] . $item["sysid"] .
+                    $item["date"];
+          }
+          break;
+        case BAKAPI_SECTION_EVENTS:
+          foreach($db[$section] as $item) {
+            $str .= $item["name"] . $item["description"] . $item["timerange"] .
+                    $item["rooms"] . $item["teachers"] . $item["classes"] .
+                    $item["show"] . $item["date"];
+          }
+          break;
+        case BAKAPI_SECTION_HOMEWORK:
+          foreach($db[$section] as $item) {
+            $str .= $item["subject"] . $item["issued"] . $item["deadline"] .
+                    $item["state"] . $item["description"];
+          }
+          break;
+        case BAKAPI_SECTION_TIMETABLE_CAPTIONS:
+          foreach($db[$section] as $item) {
+            $str .= $item["caption"] . $item["begin"] . $item["end"];
+          }
+          break;
+        case BAKAPI_SECTION_TIMETABLE_STABLE:
+          foreach($db[$section] as $item) {
+            $str .= $item["caption"] . $item["day"] . $item["type"] .
+                    $item["short"] . $item["steacher"] . $item["teacher"] .
+                    $item["shortRoom"] . $item["shortGroup"] . $item["group"] .
+                    $item["cycle"];
+          }
+          break;
+        case BAKAPI_SECTION_TIMETABLE_STABLE:
+          foreach($db[$section] as $item) {
+            $str .= $item["caption"] . $item["day"] . $item["type"] .
+                    $item["short"] . $item["steacher"] . $item["teacher"] .
+                    $item["shortRoom"] . $item["shortGroup"] . $item["group"] .
+                    $item["theme"] . $item["date"];
+          }
+          break;
+        case BAKAPI_SECTION_TIMETABLE_CAPTIONS:
+          foreach($db[$section] as $item) {
+            $str .= ((string) $item["mondayDate"]) . $item["cycle"];
+          }
+          break;
+      }
+      return \hash("sha256", $str);
+    }
   }
 }
 ?>
