6d5a937fff2b7fdec2abe192c4b8189a46f35771
Markaos <markaos@email.cz>
2016-09-01 16:57:32 +0200

    WebFrontend.php caught up with the rest

--- WebFrontend.php
+++ WebFrontend.php
@@ -1,10 +1,12 @@
 <?php
 namespace Markaos\BakAPI {
   require_once "bakapi.php";
+  require_once "Web/Util.php";
   require_once "Web/ContentBuilder.php";
   require_once "Web/BasePage.php";
   require_once "Web/Registration.php";
   require_once "Web/MainPage.php";
+  require_once "Web/TimetablePage.php";
 
   session_start();
 
@@ -19,12 +21,16 @@ namespace Markaos\BakAPI {
 
       if(isset($_GET["logout"])) {
         unset($_SESSION["UID"]);
+        unset($_SESSION["name"]);
+        unset($_SESSION["server"]);
         $this->handleRequest();
         return;
       }
 
       if(!isset($_GET["action"])) {
         \Markaos\BakAPI\Web\MainPage::handleRequest($this, $db, $_SESSION["UID"]);
+      } else if ($_GET["action"] == "timetable") {
+        \Markaos\BakAPI\Web\TimetablePage::handleRequest($this, $db, $_SESSION["UID"]);
       }
     }
 
