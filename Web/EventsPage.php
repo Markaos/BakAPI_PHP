df26073107d8ddbb4d66dbdaef728fc03016683b
Markaos <markaos@email.cz>
2016-09-03 22:26:35 +0200

    Implemented EventsPage (partially)

--- Web/EventsPage.php
+++ Web/EventsPage.php
@@ -7,22 +7,77 @@ namespace Markaos\BakAPI\Web {
       $userClient = \Markaos\BakAPI\BakAPI::getClient($this->getUID());
       $userData = $userClient->getData();
       $this->setTitle($userData["name"]);
-      $this->addContentNode(ContentBuilder::makeBlock()
-        ->addClass("valign-wrapper")
-        ->addClass("row")
-        ->setAttribute("style", "height: 90%")
-        ->addContentNode(
-          ContentBuilder::makeBlock()
-            ->addClass("valign")
-            ->addClass("col s12 m10 offset-m1 l8 offset-l2")
-            ->addContentNode(
-              ContentBuilder::makeText("h1")
-                ->addClass("center-align")
-                ->setContents("Tato část ještě není hotová")
-                ->build()
-            )
+      $data = $this->getData();
+      $events = $data[BAKAPI_SECTION_EVENTS];
+
+      $e = ContentBuilder::makeCollection();
+      foreach($events as $event) {
+        $ev = ContentBuilder::makeBlock()
+          ->addContentNode(
+            ContentBuilder::makeText()
+              ->setAttribute("style", "font-weight: 500;")
+              ->setContents($event["name"])
+              ->build()
+          )
+          ->addContentNode(ContentBuilder::makeLineBreak()->build())
+          ->addContentNode(
+            ContentBuilder::makeText()
+              ->setAttribute("style", "line-height: 1rem;")
+              ->setContents(str_replace("/chr(13)", "<br>", $event["description"]))
+              ->build()
+          );
+
+        $date = \date("j.n.Y", $event["date"]);
+        if($event["timerange"] != "") {
+          $date .= " " . $event["timerange"];
+        }
+
+        $ev->addContentNode(ContentBuilder::makeLineBreak()->build());
+        $ev->addContentNode(
+          ContentBuilder::makeText()
+            ->addClass("grey-text text-darken-2")
+            ->setContents($date)
             ->build()
-        )
+        );
+
+        if($event["classes"] != "") {
+          $ev->addContentNode(ContentBuilder::makeLineBreak()->build());
+          $ev->addContentNode(
+            ContentBuilder::makeText()
+              ->addClass("grey-text")
+              ->setContents("Třídy: " . $event["classes"])
+              ->build()
+          );
+        }
+
+        if($event["rooms"] != "") {
+          $ev->addContentNode(ContentBuilder::makeLineBreak()->build());
+          $ev->addContentNode(
+            ContentBuilder::makeText()
+              ->addClass("grey-text")
+              ->setContents("Místnosti: " . $event["rooms"])
+              ->build()
+          );
+        }
+
+        if($event["teachers"] != "") {
+          $ev->addContentNode(ContentBuilder::makeLineBreak()->build());
+          $ev->addContentNode(
+            ContentBuilder::makeText()
+              ->addClass("grey-text")
+              ->setContents("Učitelé: " . $event["teachers"])
+              ->build()
+          );
+        }
+
+        $e->addItem($ev->build());
+      }
+
+      $this->addContentNode(ContentBuilder::makeBlock()
+        ->addClass("container")
+        ->addClass("row")
+        ->setAttribute("style", "margin-bottom: 0px;")
+        ->addContentNode($e->build())
         ->build());
       $this->addPermanentMenuEntry(ContentBuilder::makeBlock("a")
         ->setAttribute("href", "?frontend=cz.markaos.bakapi.web&sync=true")
