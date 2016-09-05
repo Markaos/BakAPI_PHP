3a372b9a7ee0c42d52f618232f553137df30f52a
Markaos <markaos@email.cz>
2016-09-05 14:33:24 +0200

    Another fix for LegacyClient

--- LegacyClient.php
+++ LegacyClient.php
@@ -509,8 +509,7 @@ namespace Markaos\BakAPI {
       $correction = 0;
       $arr = array();
       for($i = 0; $i < 4; $i++) {
-        $dateInt = \strtotime(($i + $correction == 0 ? "-" : "+") .
-          \abs($i + $correction - 1) . " week monday");
+        $dateInt = \strtotime("+" . \abs($i + $correction) * 7 . " day");
         $date = \date("Ymd", $dateInt);
         $store = \Markaos\BakAPI\Util::loadPage($this->server .
           "/login.aspx?hx=" . $this->hash . "&pm=rozvrh&pmd=$date");
