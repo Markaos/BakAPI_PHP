2662d3df07fd777c1d83e196e57672d04a070239
Markaos <markaos@email.cz>
2016-09-19 22:12:29 +0200

    Fixed lesson-only highlighting

--- Web/TimetablePage.php
+++ Web/TimetablePage.php
@@ -247,6 +247,7 @@ namespace Markaos\BakAPI\Web {
               $ovRoom = $day[$id]["overlay"]["room"];
 
               if($h == "lesson_name") {
+                $ovTitle = $ovTeacher || $ovRoom;
                 $ovTeacher = false;
                 $ovRoom = false;
               } else if ($h == "none") {
